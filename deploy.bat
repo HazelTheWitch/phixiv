f9d24f966589e1469c04b69a7b6d754ab0ef5efa
Hazel Rella <hazelrella11@gmail.com>
2022-12-20 03:10:33 -0800

    fixed build script

--- deploy.bat
+++ deploy.bat
@@ -2,5 +2,5 @@
 
 cargo lambda build --release --arm64 --output-format zip
 
-aws lambda update-function-code --function-name phixiv --zip-file "fileb://./target/lambda/phixiv/bootstrap.zip"
-aws lambda update-function-code --function-name phixiv_proxy --zip-file "fileb://./target/lambda/phixiv_proxy/bootstrap.zip"
+aws lambda update-function-code --function-name phixiv --architectures "arm64" --zip-file "fileb://./target/lambda/phixiv/bootstrap.zip"
+aws lambda update-function-code --function-name phixiv_proxy --architectures "arm64" --zip-file "fileb://./target/lambda/phixiv_proxy/bootstrap.zip"
