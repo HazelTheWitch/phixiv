5a8419173e2c0e9407620b05f8892edf7f4c4573
Hazel Rella <hazelrella11@gmail.com>
2022-12-21 05:06:30 -0800

    added minification to the html returned by the main phixiv lambda

--- src/artwork.rs
+++ src/artwork.rs
@@ -1,7 +1,19 @@
+use std::string::FromUtf8Error;
+
 use serde::Serialize;
 use tera::{Context, Tera};
+use minify_html::{Cfg, minify};
+use thiserror::Error;
 
 use crate::pixiv_url::PixivResponse;
+
+#[derive(Debug, Error)]
+pub enum ArtworkError {
+    #[error("html templating error")]
+    Tera(#[from] tera::Error),
+    #[error("minifying error")]
+    Minify(#[from] FromUtf8Error),
+}
 
 #[derive(Debug, Serialize)]
 pub struct Artwork {
@@ -14,12 +26,21 @@ pub struct Artwork {
 }
 
 impl Artwork {
-    pub fn to_html(&self) -> Result<String, tera::Error> {
+    pub fn to_html(&self) -> Result<String, ArtworkError> {
         let mut tera = Tera::default();
         tera.add_raw_template("artwork.html", include_str!("../templates/artwork.html"))
             .unwrap();
 
-        tera.render("artwork.html", &Context::from_serialize(self)?)
+        let mut cfg = Cfg::new();
+        cfg.do_not_minify_doctype = true;
+        cfg.ensure_spec_compliant_unquoted_attribute_values = true;
+        cfg.keep_spaces_between_attributes = true;
+
+        let html = &tera.render("artwork.html", &Context::from_serialize(self)?)?;
+
+        let minified = minify(html.as_bytes(), &cfg);
+
+        Ok(String::from_utf8(minified)?)
     }
 }
 
