949b245edccf52245ff080f744e3c33c72e80cf2
Hazel Rella <hazelrella11@gmail.com>
2022-12-22 01:28:20 -0800

    refactored embed to be cleaner and run faster

--- src/bin/phixiv_embed.rs
+++ src/bin/phixiv_embed.rs
@@ -1,8 +1,6 @@
-use std::collections::HashMap;
-
 use lambda_http::{run, service_fn, Error, Request, RequestExt};
 use reqwest::StatusCode;
-use serde::{Deserialize, Serialize};
+use serde::Serialize;
 use serde_json::to_value;
 use thiserror::Error;
 use urlencoding::encode;
@@ -24,54 +22,43 @@ async fn main() -> Result<(), Error> {
 }
 
 #[derive(Debug, Serialize)]
-struct EmbedResponse {
-    version: String,
+struct EmbedResponse<'s> {
+    version: &'static str,
     #[serde(rename = "type")]
-    embed_type: String,
-    author_name: String,
+    embed_type: &'static str,
+    author_name: &'s str,
     author_url: String,
-    provider_name: String,
-    provider_url: String,
+    provider_name: &'static str,
+    provider_url: &'static str,
 }
 
-impl From<PixivEmbedResponse> for EmbedResponse {
-    fn from(por: PixivEmbedResponse) -> Self {
+impl<'s> EmbedResponse<'s> {
+    fn new(author_name: &'s str, author_url: String) -> Self {
         Self {
-            version: "1.0".to_string(),
-            embed_type: "rich".to_string(),
-            author_name: por.author_name,
-            author_url: por.author_url,
-            provider_name: "phixiv".into(),
-            provider_url: "https://github.com/HazelTheWitch/phixiv".into(),
+            version: "1.0",
+            embed_type: "rich",
+            author_name: author_name,
+            author_url: author_url,
+            provider_name: "phixiv",
+            provider_url: "https://github.com/HazelTheWitch/phixiv",
         }
     }
 }
 
-#[derive(Debug, Deserialize)]
-struct PixivEmbedResponse {
-    author_name: String,
-    author_url: String,
-}
-
 async fn embed_handler(request: Request) -> Result<(StatusCode, serde_json::Value), Error> {
-    // TODO: This could be optimized to not require so many allocations but I don't care right now.
-    let query_string: HashMap<String, String> = request
-        .query_string_parameters()
-        .iter()
-        .map(|(k, v)| (k.to_string(), v.to_string()))
-        .collect();
+    let query_string = request.query_string_parameters();
 
-    let (Some(author_name), Some(author_id)) = (query_string.get("n"), query_string.get("i")) else {
+    let (Some(author_name), Some(author_id)) = (query_string.first("n"), query_string.first("i")) else {
         return Err(EmbedError::InvalidAuthorParameters)?;
     };
 
-    let pixiv_embed_response = PixivEmbedResponse {
-        author_name: author_name.to_string(),
-        author_url: format!("https://www.pixiv.net/users/{}", encode(&author_id)),
-    };
+    let pixiv_embed_response = EmbedResponse::new(
+        author_name,
+        format!("https://www.pixiv.net/users/{}", encode(author_id)),
+    );
 
     Ok((
         StatusCode::OK,
-        to_value::<EmbedResponse>(pixiv_embed_response.into())?,
+        to_value::<EmbedResponse>(pixiv_embed_response)?,
     ))
 }
