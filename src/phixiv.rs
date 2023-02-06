81d1ea5b79b5bd3f104f0a44ef9fde8e4296b2b4
Hazel Rella <hazelrella11@gmail.com>
2023-02-06 10:35:54 -0800

    added automatic redirect back for bot user agents

--- src/phixiv.rs
+++ src/phixiv.rs
@@ -2,12 +2,13 @@ use std::sync::Arc;
 
 use axum::{
     extract::{Path, State},
-    middleware,
-    response::Html,
+    middleware::{self, Next},
+    response::{Html, Redirect, Response, IntoResponse},
     routing::get,
     Router,
 };
-use http::StatusCode;
+use http::{StatusCode, Request};
+use isbot::Bots;
 use tokio::sync::RwLock;
 
 use crate::{
@@ -22,8 +23,6 @@ pub async fn artwork_handler(
 ) -> Result<Html<String>, (StatusCode, String)> {
     let state = state.read().await;
 
-    tracing::info!("Access Token: {}", &state.auth.access_token);
-
     let artwork = Artwork::from_path(path, &state.auth.access_token)
         .await
         .map_err(|e| handle_error(e.into()))?;
@@ -35,11 +34,36 @@ pub async fn artwork_handler(
     ))
 }
 
+pub async fn redirect_middleware<B>(
+    request: Request<B>,
+    next: Next<B>,
+) -> Response {
+    let bots = Bots::default();
+
+    if let Some(Ok(user_agent)) = request.headers().get("User-Agent").map(|h| h.to_str()) {
+        if !bots.is_bot(user_agent) {
+            tracing::info!("Non-bot request, redirecting to pixiv.");
+
+            let uri = request.uri();
+
+            let path_and_query = match uri.path_and_query() {
+                Some(path_and_query) => path_and_query.as_str(),
+                None => "",
+            };
+
+            return Redirect::temporary(&format!("http://www.pixiv.net{}", path_and_query)).into_response();
+        }
+    }
+
+    next.run(request).await
+}
+
 pub fn phixiv_router(state: Arc<RwLock<PhixivState>>) -> Router {
     Router::new()
         .route("/:language/artworks/:id", get(artwork_handler))
         .route("/artworks/:id", get(artwork_handler))
         .fallback(pixiv_redirect)
         .with_state(state.clone())
-        .route_layer(middleware::from_fn_with_state(state, auth_middleware))
+        .layer(middleware::from_fn_with_state(state, auth_middleware))
+        .layer(middleware::from_fn(redirect_middleware))
 }
