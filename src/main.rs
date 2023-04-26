d44dd8b570401511447fe1fb9f11037e056b9845
Hazel Rella <hazelrella11@gmail.com>
2023-04-26 04:17:25 -0700

    jemallocator test

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,13 @@ use tokio::sync::RwLock;
 
 use tower_http::normalize_path::NormalizePathLayer;
 
+#[cfg(not(target_env = "msvc"))]
+use tikv_jemallocator::Jemalloc;
+
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
+
 #[tokio::main]
 async fn main() {
     dotenvy::dotenv().ok();
