b8271033c1fae3ace428babf6d4123d41bb76de5
Hazel Rella <hazelrella11@gmail.com>
2023-04-29 14:19:08 -0700

    added prometheus layer

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{env, sync::Arc};
 
 use axum::{routing::get, Router};
+use axum_prometheus::PrometheusMetricLayer;
 use phixiv::{
     embed::embed_handler,
     phixiv::phixiv_router,
@@ -31,12 +32,16 @@ async fn main() {
     let proxy = proxy_router(state.clone());
     let direct = direct_router(state.clone());
 
+    let (prometheus_layer, metric_handle) = PrometheusMetricLayer::pair();
+
     let app = Router::new()
         .merge(phixiv)
         .route("/e", get(embed_handler))
         .nest("/i", proxy)
         .nest("/d", direct)
+        .route("/metrics", get(|| async move { metric_handle.render() }))
         .fallback(pixiv_redirect)
+        .layer(prometheus_layer)
         .layer(NormalizePathLayer::trim_trailing_slash());
 
     let addr = format!("[::]:{}", env::var("PORT").unwrap_or("3000".to_owned()))
