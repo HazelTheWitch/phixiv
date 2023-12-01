d42b2c67a671d778afaf1859aac7e0a3f2a4e28d
Hazel Rella <hazelrella11@gmail.com>
2023-11-30 21:17:49 -0800

    added discontinued message to embed

--- src/oembed.rs
+++ src/oembed.rs
@@ -28,8 +28,8 @@ impl EmbedResponse {
             embed_type: "rich",
             author_name,
             author_url,
-            provider_name: "phixiv",
-            provider_url: "https://github.com/HazelTheWitch/phixiv",
+            provider_name: "PHIXIV WILL BE DISCONTINUED 01/01/2024",
+            provider_url: "https://github.com/HazelTheWitch/phixiv/blob/main/GOODBYE.md",
         }
     }
 }
